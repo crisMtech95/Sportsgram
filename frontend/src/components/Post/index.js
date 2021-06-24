33c215673a6440a5be6328bee0b936022fa8f262
crisMtech95 <cmg.95.col@hotmail.com>
2021-06-24 17:25:28 -0400

    some changes

--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -5,67 +5,7 @@ import { delPost, editPost } from '../../store/images'
 import { useDispatch, useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 import { addComment } from '../../store/comments'
-
-function EditPostForm ({img}) {
-    const sessionUser = useSelector(state => state.session.user)
-    const [imageUrl, setImageUrl] = useState("")
-    const [content, setContent] = useState("")
-    const [sport, setSport] = useState("")
-    const [newImg, setnewImg] = useState("")
-    const [errors, setErrors] = useState([])
-    const dispatch = useDispatch()
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-         dispatch(editPost({ userId: sessionUser.id, imageUrl, content, sport, id: img.id}));
-                    // .catch(async (res) => {
-                    // const data = await res.json()
-                    // if (data && data.errors) setErrors(data.errors)
-        // })
-
-    }
-
-    return (
-            <div className="mainEdit__container">
-                <form onSubmit={onSubmit} className="edit__form">
-                    <ul>
-                        {errors.map((err, i) => <li key={i}>{err}</li>)}
-                    </ul>
-                    <div className="editPost__labels">
-                        <label /> Image Url
-                    </div>
-                        <input className="edit__input"
-                        type="text"
-                        required
-                        value={imageUrl}
-                        onChange={e => setImageUrl(e.target.value)}
-                        />
-                    <div className="editPost__labels">
-                        <label /> content
-                    </div>
-                        <textarea className="edit__textarea"
-                        type="text"
-                        required
-                        value={content}
-                        onChange={e => setContent(e.target.value)}
-                        />
-                        <div className="editPost__labels">
-                            <label /> sport/title
-                        </div>
-                        <input className="edit__input"
-                        type="text"
-                        required
-                        value={sport}
-                        onChange={e => setSport(e.target.value)}
-                        />
-                        <div className="edit__btndiv">
-                    <button type="submit" className="edit__btn">Edit Post</button>
-                        </div>
-                </form>
-            </div>
-    )
-}
-
+import EditPostForm from '../EditPostForm'
 
 export default function Post ({ img }) {
     const sessionUser = useSelector(state => state.session.user)
