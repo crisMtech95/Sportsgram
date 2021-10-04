f297be8c826aa3da0da113fcfd347928dccef0cc
crisMtech95 <cmg.95.col@hotmail.com>
2021-10-04 15:40:04 -0400

    added some seeder objects to images

--- backend/db/seeders/20210622003717-images.js
+++ backend/db/seeders/20210622003717-images.js
@@ -6,7 +6,7 @@ module.exports = {
         {
         imageUrl: "https://icdn.football-espana.net/wp-content/uploads/2020/11/merlin_153612873_5bb119b9-8972-4087-b4fd-371cab8c5ba2-superJumbo.jpg",
         sport: "soccer",
-        userId: 3,
+        userId: 1,
         content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
         createdAt: "2021-06-21 17:38:46.592231-04",
         updatedAt: "2021-06-21 17:38:46.592231-04"
@@ -14,7 +14,7 @@ module.exports = {
         {
         imageUrl: "http://cdn.cnn.com/cnnnext/dam/assets/130624174959-50-surf-spots-the-box.jpg",
         sport: "surfing",
-        userId: 3,
+        userId: 1,
         content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
         createdAt: "2021-06-21 17:38:46.592231-04",
         updatedAt: "2021-06-21 17:38:46.592231-04"
@@ -22,13 +22,77 @@ module.exports = {
         {
         imageUrl: "https://mk0f57hourscom1d61eq.kinstacdn.com/wp-content/uploads/2020/09/Rock-climbing-Canada.jpg",
         sport: "rock climbing",
-        userId: 3,
+        userId: 1,
         content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
         createdAt: "2021-06-21 17:38:46.592231-04",
         updatedAt: "2021-06-21 17:38:46.592231-04"
       },
         {
         imageUrl: "https://cdn.theathletic.com/app/uploads/2019/10/17032021/ronaldinho1-710x1024.png",
+        sport: "soccer",
+        userId: 1,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://princerupertstower.com/wp-content/uploads/getty-images/2020/10/1228964635.jpeg",
+        sport: "soccer",
+        userId: 2,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://i0.heartyhosting.com/www.surfer.com/wp-content/uploads/2015/08/wallpaperfi.jpg?resize=1200%2C600&ssl=1",
+        sport: "surfing",
+        userId: 2,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://wallpaperaccess.com/full/347681.jpg",
+        sport: "rock climbing",
+        userId: 2,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://www.si.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY5NTg0MTYyOTQ3MjEzMTI1/magic-johnson-action-portrait_header.jpg",
+        sport: "soccer",
+        userId: 2,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F210420110433-02-kobe-bryant-michael-jordan-restricted.jpg",
+        sport: "soccer",
+        userId: 3,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://s.france24.com/media/display/7b9032e0-78c1-11eb-a167-005056a98db9/w:1280/p:16x9/063_1292253394.webp",
+        sport: "surfing",
+        userId: 3,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://www.tennisworldusa.org/imge/104205/young-rafael-nadal-losing-at-major-event-is-never-easy-.jpg",
+        sport: "rock climbing",
+        userId: 3,
+        content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
+        createdAt: "2021-06-21 17:38:46.592231-04",
+        updatedAt: "2021-06-21 17:38:46.592231-04"
+      },
+        {
+        imageUrl: "https://m.media-amazon.com/images/I/71sRrdP7vFL._AC_SL1500_.jpg",
         sport: "soccer",
         userId: 3,
         content: "The England striker has not publicly stated that he wants to leave the club but he has made no secret about his desire to want to win trophies.Kane missed out on his first medal when Spurs were beaten by City in the Carabao Cup in April, while they have also missed out on qualification for the Champions League.He also revealed in an interview with former England international Gary Neville that he",
